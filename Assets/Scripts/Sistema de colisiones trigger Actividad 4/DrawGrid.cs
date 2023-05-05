c5d930af8c048515ddff51f32440bd6d6625a8a3
fekox <86255202+fekox@users.noreply.github.com>
2023-05-05 12:30:28 -0300

    Fix the draw grid script

--- Assets/Scripts/Sistema de colisiones trigger Actividad 4/DrawGrid.cs
+++ Assets/Scripts/Sistema de colisiones trigger Actividad 4/DrawGrid.cs
@@ -6,9 +6,45 @@ using UnityEngine.SocialPlatforms.GameCenter;
 
 public class DrawGrid : MonoBehaviour
 {
-    void OnDrawGizmosSelected()
+    [Header("Setup")]
+    
+    [SerializeField] private float delta = 0.2f;
+    
+    private const int maxPoints = 10;
+
+    public Vec3[,,] grid = new Vec3[maxPoints, maxPoints, maxPoints];
+
+    public bool drawGrid = true;
+
+    void Start()
     {
-        Gizmos.color = new Color(1, 0.5f, 0.5f, 0.5f);
-        Gizmos.DrawWireCube(transform.position, new Vec3(1, 0, 1));
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                for (int k = 0; k < grid.GetLength(2); k++)
+                {
+                    grid[i, j, k] = new Vec3(i, j, k) * delta;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (drawGrid == true) 
+        {
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    for (int k = 0; k < grid.GetLength(2); k++)
+                    {
+                        Gizmos.color = Color.white;
+                        Gizmos.DrawSphere(grid[i, j, k], 0.02f);
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Sistema de colisiones trigger Actividad 4/TriggerColisions.cs
+++ Assets/Scripts/Sistema de colisiones trigger Actividad 4/TriggerColisions.cs
@@ -9,30 +9,10 @@ public class TriggerColisions : MonoBehaviour
 {
     [SerializeField] private GameObject plane;
 
-    //First Plane
-    static Vec3 vecA1 = new Vec3(0, 1, 0);
-    static Vec3 vecA2 = new Vec3(1, 2, 0);
-    static Vec3 vecA3 = new Vec3(-1, 2, 0);
-
-    private MrPlane Plane1 = new MrPlane(vecA1, vecA2, vecA3);
 
     void OnDrawGizmosSelected()
     {
-        //First Plane
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(vecA1, 0.1f);
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(vecA3, vecA1);
 
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(vecA2, vecA1);
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(vecA2, 0.1f);
-
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(vecA2, vecA3);
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(vecA3, 0.1f);
     }
 
 }
