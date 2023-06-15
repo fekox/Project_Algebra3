e7c8aea9feaf6d67cf3a0ef27044e727c4e7ba1a
fekox <facusantos123@gmail.com>
2023-06-15 19:13:09 -0300

    Added property functions

--- Assets/Scripts/Quaternions/Quaternions.cs
+++ Assets/Scripts/Quaternions/Quaternions.cs
@@ -7,6 +7,7 @@ using UnityEngine.Internal;
 using UnityEngine.Scripting;
 using UnityEngine;
 using CustomMath;
+using Unity.VisualScripting;
 
 Quaternion q;
 
@@ -42,5 +43,69 @@ public struct MrQuaternion
     }
     #endregion
 
-    private static readonly MrQuaternion identityQuaternion = new MrQuaternion(0f, 0f, 0f, 1f);
+    #region Properties
+    public static MrQuaternion Identity //Returns a quaternuin that corresponds to ¨not rotated¨.
+    {                                   //The object is perfectly aligned with the world or parent axes.
+        get 
+        { 
+            return new MrQuaternion(0f, 0f, 0f, 1f); 
+        }
+    }
+
+    public bool IsIdentity //Returns true if whether the given quaternion is equals to the quaternion identity.
+    {
+        get 
+        {
+            return x == 0f && y == 0f && z == 0f && w == 1f;
+        }
+    }
+
+    public float this[int index] //Access the x, y, z, w components using [0], [1], [2], [3] respectively.
+    {                            //Example: ( p[3] = 0.5f; the same as p.w = 0.5 ).
+                                 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            switch (index) 
+            { 
+                case 0: return x; 
+
+                case 1: return y; 
+
+                case 2: return z;
+
+                case 3: return w;
+
+                default: throw new IndexOutOfRangeException("Invalid Quaternion index!");
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        set 
+        {
+            switch(index) 
+            {
+                case 0: x = value; break;
+
+                case 1: y = value; break;
+
+                case 2: z = value; break;
+
+                case 3: w = value; break;
+
+                default: throw new IndexOutOfRangeException("Invalid Quaternion index!");
+            }
+        }
+    }
+
+    public MrQuaternion Zero //Return a quaternion whose values are (0, 0, 0, 0).
+    { 
+        get 
+        {
+            return new MrQuaternion(0f, 0f, 0f, 0f);
+        } 
+    }
+    #endregion
+
+
 }
