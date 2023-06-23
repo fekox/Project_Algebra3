2b2b84f2e856fb9a55613488f15e4f64918b8997
fekox <facusantos123@gmail.com>
2023-06-23 11:51:43 -0300

    Add the normalize and dot functions

--- Assets/Scripts/Quaternions/Quaternions.cs
+++ Assets/Scripts/Quaternions/Quaternions.cs
@@ -8,6 +8,9 @@ using UnityEngine.Scripting;
 using UnityEngine;
 using CustomMath;
 using Unity.VisualScripting;
+using System.Security.Principal;
+
+UnityEngine.Quaternion
 
 Quaternion q;
 
@@ -44,33 +47,18 @@ public struct MrQuaternion
     #endregion
 
     #region Properties
-    public static MrQuaternion Identity //Returns a quaternuin that corresponds to ¨not rotated¨.
-    {                                   //The object is perfectly aligned with the world or parent axes.
-        get 
-        { 
-            return new MrQuaternion(0f, 0f, 0f, 1f); 
-        }
-    }
-
-    public bool IsIdentity //Returns true if whether the given quaternion is equals to the quaternion identity.
-    {
-        get 
-        {
-            return x == 0f && y == 0f && z == 0f && w == 1f;
-        }
-    }
 
     public float this[int index] //Access the x, y, z, w components using [0], [1], [2], [3] respectively.
     {                            //Example: ( p[3] = 0.5f; the same as p.w = 0.5 ).
-                                 
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
-            switch (index) 
-            { 
-                case 0: return x; 
+            switch (index)
+            {
+                case 0: return x;
 
-                case 1: return y; 
+                case 1: return y;
 
                 case 2: return z;
 
@@ -81,9 +69,9 @@ public struct MrQuaternion
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        set 
+        set
         {
-            switch(index) 
+            switch (index)
             {
                 case 0: x = value; break;
 
@@ -98,7 +86,32 @@ public struct MrQuaternion
         }
     }
 
-    public MrQuaternion Zero //Return a quaternion whose values are (0, 0, 0, 0).
+    public static MrQuaternion identity //Returns a quaternion that corresponds to ¨not rotated¨.
+    {                                   //The object is perfectly aligned with the world or parent axes.
+        get 
+        { 
+            return new MrQuaternion(0f, 0f, 0f, 1f); 
+        }
+    }
+
+    public MrQuaternion normalized //Returns a quaternion normalizes.
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get
+        {
+            return Normalize(this);
+        }
+    }
+
+    public bool isIdentity //Returns true if whether the given quaternion is equals to the quaternion identity.
+    {
+        get 
+        {
+            return x == 0f && y == 0f && z == 0f && w == 1f;
+        }
+    }
+
+    public MrQuaternion zero //Return a quaternion whose values are (0, 0, 0, 0).
     { 
         get 
         {
@@ -106,6 +119,31 @@ public struct MrQuaternion
         } 
     }
     #endregion
+
+    #region Functions
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static MrQuaternion Normalize(MrQuaternion q)
+    {
+        float num = Mathf.Sqrt(Dot(q, q)); //Obtain the result of the square root of the dot product of the quaternion.
+        
+        if (num < Mathf.Epsilon) //If the result is less than epsilon. (A tiny floating point value).
+        {
+            return identity; //Return MrQuaternion(0f, 0f, 0f, 1f). 
+        }
+
+        return new MrQuaternion(q.x / num, q.y / num, q.z / num, q.w / num); //If not, a quaternion divided by the previously obtained result is returned.
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Dot(MrQuaternion a, MrQuaternion b)
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;//Return dot product by two quaternions.
+    }
+
+    #endregion
+
+
 
 
 }
